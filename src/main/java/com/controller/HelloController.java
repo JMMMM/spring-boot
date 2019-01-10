87980ddaf281f1a552ebfd9b4d7d4c0cbc09b820
wujiaming <wujiaming@iplas.com.cn>
2019-01-10 10:48:23 +0800

    123

--- src/main/java/com/controller/HelloController.java
+++ src/main/java/com/controller/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -35,7 +36,7 @@ public class HelloController {
     }
 
     @RequestMapping("/search")
-    public Object search(@PageableDefault Pageable pageable, String keyword) {
+    public Object search(@PageableDefault Pageable pageable, @RequestParam(defaultValue = "") String keyword) {
         return elasticsearchService.demo(pageable,keyword);
     }
 }
--- src/main/java/com/elasticsearch/ElasticsearchServiceImpl.java
+++ src/main/java/com/elasticsearch/ElasticsearchServiceImpl.java
@@ -1,5 +1,6 @@
 package com.elasticsearch;
 
+import com.alibaba.druid.util.StringUtils;
 import com.elasticsearch.domain.Products;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -8,10 +9,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.data.elasticsearch.core.query.SearchQuery;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Service;
 
-import static org.elasticsearch.index.query.QueryBuilders.matchAllQuery;
 import static org.elasticsearch.index.query.QueryBuilders.queryStringQuery;
 
 @Service
@@ -20,9 +19,11 @@ public class ElasticsearchServiceImpl {
     private ElasticsearchTemplate elasticsearchTemplate;
 
     public Page<Products> demo(Pageable pageAble, String keyword) {
-        SearchQuery searchQuery = new NativeSearchQueryBuilder().withQuery(
-                queryStringQuery(keyword)).withPageable(pageAble)
-                .build();
+        NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
+        if(StringUtils.isEmpty(keyword)){
+            nativeSearchQueryBuilder.withQuery(queryStringQuery(keyword));
+        }
+        SearchQuery searchQuery = nativeSearchQueryBuilder.withPageable(pageAble).build();
         Page<Products> results = elasticsearchTemplate.queryForPage(searchQuery, Products.class);
         return results;
     }
