cdd13550ccc2b01dbc234fef7b18ac382c88c913
wujiaming <wujiaming@iplas.com.cn>
2019-01-10 11:56:43 +0800

    123

--- src/main/java/com/dbconfig/MultipleDataSource.java
+++ src/main/java/com/dbconfig/MultipleDataSource.java
@@ -1,15 +1,20 @@
 package com.dbconfig;
 
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
-public class MultipleDataSource extends AbstractRoutingDataSource{
+import javax.sql.DataSource;
+
+public class MultipleDataSource extends AbstractRoutingDataSource {
     @Override
     protected Object determineCurrentLookupKey() {
         Object result = ChooseDataSourceUtils.chooseDataSource();
         Boolean rs = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
-        System.out.println("MultipleDataSource:"+rs);
-        return result;
+        System.out.println("MultipleDataSource:" + rs);
+        DruidDataSource dataSource =  (DruidDataSource)this.resolveSpecifiedDataSource(result);
+        if(dataSource.isKeepAlive()) return result;
+        else return "master";
     }
 }
