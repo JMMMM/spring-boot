c6502e60ca4d1ada3a4bf4fa7b580e15b7d4a8c5
wujiaming <wujiaming@iplas.com.cn>
2019-03-07 16:17:28 +0800

    123

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -2,6 +2,8 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by jimmy on 2017/5/7.
