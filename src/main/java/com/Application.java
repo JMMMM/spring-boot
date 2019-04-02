cd396ddb8b65259f2e5ae38f8cdc402d1c547372
wujiaming <wujiaming@iplas.com.cn>
2019-04-02 09:41:23 +0800

    123

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -11,7 +11,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        System.setProperty("es.set.netty.runtime.available.processors", "false");
+//        System.setProperty("es.set.netty.runtime.available.processors", "false");
         SpringApplication.run(Application.class,args);
     }
 }
